7c71f501ca281f7df9294ffffe9fbc70a4425425
yuta <yuta.uchida@gmail.com>
2014-03-01 16:10:52 +0900

    Finish sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include SessionsHelper
+
+    # CSRF脆弱性の対策のために、サインアウトさせる。
+	def handle_unverified_request
+	  sign_out
+	  super
+	end
+
+
+
 end
